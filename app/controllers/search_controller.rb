e0cc414688835afb050e17760ca526573f33dd07
Ahmet Abdi <ahmetabdi@gmail.com>
2016-09-05 12:26:58 +0100

    Search update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,15 @@
 class SearchController < ApplicationController
 	def autocomplete
-    render json: Searchkick.search(params[:query],
-    	index_name: [MagicCard, MagicSet],
-    	fields: [:name, :text],
-    	match: :word_start,
-      limit: 10,
-      load: false,
-      misspellings: {below: 5}
-    ).as_json
+    render json: MagicCard.search(params[:query], fields: ["name", "text"], load: false).as_json
+      # aggs: {color: {order: {"_term" => "asc"}}}
+    # ).as_json
+    # render json: Searchkick.search(params[:query],
+    # 	index_name: [MagicCard, MagicSet],
+    # 	fields: [:name, :text],
+    # 	match: :word_start,
+    #   limit: 10,
+    #   load: false,
+    #   misspellings: {below: 5}
+    # ).as_json
   end
 end
