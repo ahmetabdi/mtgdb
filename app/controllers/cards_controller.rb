764735416b4367e7e8dca02e233f520ef559e009
Ahmet Abdi <ahmetabdi@gmail.com>
2016-09-05 09:21:56 +0100

    also seen in

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   def show
     @card = MagicCard.friendly.find(params[:id])
+    @also_seen_in = MagicCard.where(name: @card.name).where.not(id: @card.id)
   end
 end
