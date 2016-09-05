8169ab2c7bc422ba251ec8507fc375b2ad919a86
Ahmet Abdi <ahmetabdi@gmail.com>
2016-09-05 12:36:45 +0100

    Set limit to 20

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -104,6 +104,7 @@ $(document).on('ready page:load', function(event) {
 		  name: 'best-pictures',
 		  displayKey: 'name',
 		  source: bloodQuery,
+      limit: 20,
 		  //source: function(query, syncResults, asyncResults) {
       //  console.log(query);
       //  $.get('/search?query=' + encodeURIComponent(query), function(data) {
