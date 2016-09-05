0bfe0074aa9c36ff0e86afa03e7f50010ec2048c
Steven Wake <steven@kyanmedia.com>
2016-09-05 11:38:03 +0100

    Searches card text after name
    • Added text to fields (app/controllers/search_controller.rb)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 	def autocomplete
     render json: Searchkick.search(params[:query],
     	index_name: [MagicCard, MagicSet],
-    	fields: [:name],
+    	fields: [:name, :text],
     	match: :word_start,
       limit: 10,
       load: false,
